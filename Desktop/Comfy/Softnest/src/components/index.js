06c74878676ceb8e50a36f4a95dd1b277f8ab59c
Sifiso Welcome Bestenbier <sifisowelcm12@gmail.com>
2026-07-11 10:52:47 +0200

    Adding a navbar

--- Desktop/Comfy/Softnest/src/components/index.js
+++ Desktop/Comfy/Softnest/src/components/index.js
@@ -1,3 +1,5 @@
 export { default as FormInput } from './FormInput'
 export { default as SubmitBtn } from './SubmitBtn'
 export { default as Header } from './Header'
+export { default as NavBar } from './NavBar'
+export { default as NavLinks } from './NavLinks'
--- Desktop/Comfy/Softnest/src/pages/HomeLayout.jsx
+++ Desktop/Comfy/Softnest/src/pages/HomeLayout.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import { Header } from '../components'
+import { NavBar } from '../components'
 const HomeLayout = () => {
   return (
     <>
       <Header />
+      <NavBar />
       <nav>
         <span className="text-4xl text-primary">Softnest</span>
       </nav>
