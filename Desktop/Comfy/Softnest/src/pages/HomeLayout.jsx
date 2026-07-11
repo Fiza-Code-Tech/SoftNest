e71c14441a68ff747f7dfdd828bb26f9f558a17a
Sifiso Welcome Bestenbier <sifisowelcm12@gmail.com>
2026-07-11 09:40:39 +0200

    Adding header

--- Desktop/Comfy/Softnest/src/pages/HomeLayout.jsx
+++ Desktop/Comfy/Softnest/src/pages/HomeLayout.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-
+import { Header } from '../components'
 const HomeLayout = () => {
   return (
     <>
+      <Header />
       <nav>
         <span className="text-4xl text-primary">Softnest</span>
       </nav>
-      <Outlet />
+      <section className="align-element py-20">
+        <Outlet />
+      </section>
     </>
   )
 }
