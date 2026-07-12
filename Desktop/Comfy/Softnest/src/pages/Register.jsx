9342adc703944d658608add0892efa8b1a1624d7
Sifiso Welcome Bestenbier <sifisowelcm12@gmail.com>
2026-07-12 14:23:16 +0200

    Adding the filter select input

--- Desktop/Comfy/Softnest/src/pages/Register.jsx
+++ Desktop/Comfy/Softnest/src/pages/Register.jsx
@@ -9,8 +9,8 @@ const Register = () => {
         className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4"
       >
         <h4 className="text-center text-3xl font-bold">Register</h4>
-        <FormInput type="text" label="username" name="usename" />
-        <FormInput type="email" label="usename" name="username" />
+        <FormInput type="text" label="username" name="username" />
+        <FormInput type="email" label="username" name="username" />
         <FormInput type="password" label="password" name="password" />
         <div className="mt-4">
           <SubmitBtn text="register" />
